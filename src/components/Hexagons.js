59f2a3aa7b1b48f871657a727700e7ca06b97e8e
Muhammad Wage Juli Saputra <wage.muhammad13@gmail.com>
2018-12-03 19:07:51 +0700

    update font-awesome for each menu

--- src/components/Hexagons.js
+++ src/components/Hexagons.js
@@ -4,6 +4,14 @@ import Header from './Header';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faUserTie } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faArchway } from '@fortawesome/free-solid-svg-icons';
+import { faCloudDownloadAlt } from '@fortawesome/free-solid-svg-icons';
+import { faImages } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 class Hexagons extends Component{
     constructor(){
@@ -14,55 +22,64 @@ class Hexagons extends Component{
                     id: 1,
                     color: '#009543',
                     text: 'BERANDA',
-                    link: ''
+                    link: '',
+                    faicon: <FontAwesomeIcon icon={faHome}/>
                 },
                 {
                     id: 2,
                     color: '#029142',
                     text: 'PROFIL',
-                    link: 'profile'
+                    link: 'profile',
+                    faicon: <FontAwesomeIcon icon={faUserTie}/>
                 },
                 {
                     id: 3,
                     color: '#058c41',
                     text: 'PELAYANAN',
-                    link: 'layanan'
+                    link: 'layanan',
+                    faicon: <FontAwesomeIcon icon={faUserCircle}/>
                 },
                 {
                     id: 4,
                     color: '#037234',
                     text: 'PENGADUAN',
-                    link: 'aduan'
+                    link: 'aduan',
+                    faicon: <FontAwesomeIcon icon={faArchway}/>
                 },
                 {
                     id: 5,
                     color: '#066d33',
                     text: 'UNDUH',
-                    link: 'unduh'
+                    link: 'unduh',
+                    faicon: <FontAwesomeIcon icon={faCloudDownloadAlt}/>
                 },
                 {
                     id: 6,
                     color: '#086832',
                     text: 'GALERI',
-                    link: 'galeri'
+                    link: 'galeri',
+                    faicon: <FontAwesomeIcon icon={faImages}/>
                 },
                 {
                     id: 7,
                     color: '#0c6834',
                     text: 'INFORMASI SPASIAL',
-                    link: 'informasi'
+                    link: 'informasi',
+                    icon: <FontAwesomeIcon icon={faInfoCircle}/>
                 },
                 {
                     id: 8,
                     color: '#024921',
                     text: 'KONTAK ',
-                    link: 'kontak '
+                    link: 'kontak ',
+                    faicon: <FontAwesomeIcon icon={faPhoneSquare}/>
                 },
                 {
                     id: 9,
                     color: '#044420',
                     text: 'LOGIN',
-                    link: 'login'
+                    link: 'login',
+                    faicon: <FontAwesomeIcon icon={faSignInAlt}/>
                 },
                
             ]
@@ -81,7 +98,7 @@ class Hexagons extends Component{
                                         <div className="hexIn">
                                             <Link to={list_menu.link} className="hexLink" style={{backgroundColor:list_menu.color}}>
                                                 {/* <img src={pict} alt=""/> */}
-                                                <FontAwesomeIcon icon={faHome}/>
+                                                {list_menu.faicon}
                                                 <p>{list_menu.text}</p>
                                             </Link>
                                         </div>
